873243867b6a420188cf04f1c1bd2a52b1561ad2
Patchoy1978 <pacho_julian@hotmail.com>
2024-11-18 13:17:48 +0100

    v1.1.4 codigo listo y comentado

--- InventariosTotales/inventarios_productos.py
+++ InventariosTotales/inventarios_productos.py
@@ -1,90 +1,156 @@
+# Importa la clase ProductosAlmacenados desde el módulo correspondiente
 from ProductosTotales.prod_almacen import ProductosAlmacenados
 
+# Clase que maneja el inventario de productos
 class Inventario():
     
     def __init__(self):
         
-        self._inventarioProd = []
+        """
+        Inicializa una instancia de la clase Inventario.
+        Se crea un atributo protegido _inventarioProd, que almacenará una lista de objetos
+        de productos gestionados en el inventario.
+        """
+        self._inventarioProd = [] # Lista vacía para almacenar los productos.
         
+    # Método para agregar productos al inventario
     def agregar_productos_Inventario(self, producto):
-            
+        
+        """
+        Agrega un producto al inventario.
+        
+        Parámetros:
+            producto (ProductosAlmacenados): Objeto que representa un producto.
+        
+        Reglas:
+        - Si un producto con el mismo nombre ya está en el inventario, lanza una excepción.
+        """    
         for prod in self._inventarioProd:
             
+            # Compara el nombre del producto a agregar con los nombres de productos existentes.
             if prod.get_nombre() == producto.get_nombre():
                 
+                # Si el producto ya está en el inventario, lanza una excepción
                 raise ValueError ("\nEl producto ya esta en el inventario")
             
-            break
-                
+            break # Solo necesita verificar una vez si el producto ya está presente.
+        
+        # Agrega el nuevo producto a la lista del inventario        
         self._inventarioProd.append(producto)
                                     
+    # Método para eliminar un producto del inventario por nombre
     def eliminar_producto(self, nombre):
         
+        """
+        Elimina un producto del inventario en base a su nombre.
+        
+        Parámetros:
+            nombre (str): Nombre del producto a eliminar.
+        
+        Reglas:
+        - Si el producto no existe, lanza una excepción.
+        - Solicita confirmación al usuario antes de eliminar el producto.
+        """
         try:
         
             for prod in self._inventarioProd:
                 
+                # Compara los nombres de los productos, ignorando mayúsculas y minúsculas
                 if prod.get_nombre().title() == nombre.title():
                     
                     producto_encontrado = prod
                     
                     if producto_encontrado:
-
+                        
+                        # Si se encuentra, solicita confirmación antes de eliminar.
                         while True:
                             
                             opcion = input("\nSeguro que quieres eliminar el prducto (S/N): ").upper()
                             
-                            if opcion == 'S':
+                            if opcion == 'S': # Si el usuario confirma:
                                 
-                                self._inventarioProd.remove(producto_encontrado)
+                                self._inventarioProd.remove(producto_encontrado) # Elimina el producto.
                                 
                                 print(f"\nEl producto {nombre} ha sido eliminado correctamente")
                                 return
                             
+                            # Si el usuario cancela, muestra un mensaje.
                             print("\nOperacion Cancelada")
                             return
-                    
+            
+            # Si el producto no se encuentra, lanza una excepción        
             raise ValueError(f"\nEl producto {nombre} no esta en el inventario")
             
         except ValueError as e:
             
+             # Captura y muestra errores relacionados con la operación.
             print(f"{e}")
             
+    # Método para mostrar todos los productos en el inventario
     def mostrar_Inventario(self):
         
+        """
+        Muestra todos los productos en el inventario en formato de tabla.
+        
+        Reglas:
+        - Si el inventario está vacío, lanza una excepción.
+        """
         try:
             
+            # Verifica si el inventario está vacío.
             if not self._inventarioProd:
                 
+                # Si el inventario está vacío, lanza una excepción
                 raise ValueError("\nNo hay productos en el inventario para mostrar\n")
             
+            # Imprime una tabla con los encabezados y los datos de los productos.
             print("\n\033[1mEl Inventario Contiene Estos Articulos\033[0m\n")
             print(f"{'-'*70}")
             print(f"{'CATEGORIA':<20} {'NOMBRE ARTICULO':<30} {'CANTIDAD':<10} {'PRECIO':<10}")
             print(f"{'-'*70}")
         
+            # Recorre cada producto en el inventario y lo muestra en la tabla.
             for prod in self._inventarioProd:
                 
                 print(f"{prod.get_categoria():<20} {prod.get_nombre():<30} {prod.get_cantidad():<10} {prod.get_precio():<10.2f}")
                 
         except ValueError as e:
             
+            # Muestra un mensaje de error si el inventario está vacío.
             print(f"{e}")
     
+    # Método para buscar productos en el inventario
     def buscar_producto(self, nombre, categoria):
 
+        """
+        Busca un producto en el inventario por nombre o categoría.
+        
+        Parámetros:
+            nombre (str): Nombre del producto a buscar.
+            categoria (str): Categoría del producto a buscar.
+        
+        Reglas:
+        - Si el inventario está vacío, lanza una excepción.
+        - Si no encuentra ningún producto, lanza una excepción.
+        
+        Retorno:
+            Retorna el producto encontrado o lanza una excepción si no se encuentra.
+        """
         if not self._inventarioProd:
             
-            raise ValueError("\nNo hay productos en el inventario para realizar una busqueda\n")     
+            # Si el inventario está vacío, no es posible buscar productos.
+            raise ValueError("\nNo hay productos en el inventario para realizar una busqueda\n")
             
-        productoEncontrado = False
+        productoEncontrado = False # Variable para rastrear si se encuentra un producto.
     
         for prod in self._inventarioProd:
-        
+            
+            # Verifica si el producto coincide por nombre o categoría.
             if prod.get_nombre() == nombre or prod.get_categoria() == categoria:
                 
                 if not productoEncontrado:
                 
+                    # Muestra encabezados solo la primera vez que encuentra un producto.
                     print("\n\033[1mHas Encontrado Estos Articulos en el Inventario\033[0m\n")
                     print(f"{'-'*70}")
                     print(f"{'CATEGORIA':<20} {'NOMBRE ARTICULO':<30} {'CANTIDAD':<10} {'PRECIO':<10}")
@@ -92,10 +158,12 @@ class Inventario():
                     
                 productoEncontrado = True
                 
+                # Muestra los detalles del producto encontrado.
                 print(f"{prod.get_categoria():<20} {prod.get_nombre():<30} {prod.get_cantidad():<10} {prod.get_precio():<10.2f}")
             
                 return prod
-            
+        
+        # Si no encuentra ningún producto, lanza una excepción.    
         if not productoEncontrado:
             
             if nombre:
@@ -106,21 +174,33 @@ class Inventario():
                 
                 raise ValueError(f"\nLa categoria {categoria} no esta en el inventario actual\n")
         
+    # Métodos para modificar propiedades específicas de los productos (nombre, categoría, precio, cantidad).
     def modficar_nombres_inventario(self, nombre):
         
+        """
+        Modifica el nombre de un producto específico.
+        
+        Parámetros:
+            nombre (str): Nombre actual del producto a modificar.
+        
+        Retorno:
+            Mensaje de confirmación de la operación.
+        """
         try:
             
+             # Verifica que el inventario no esté vacío.
             if not self._inventarioProd:
                 
                 raise ValueError("\nEL inventario esta vacio")
             
-            prod_encontrado = self.buscar_producto(nombre, None)
+            prod_encontrado = self.buscar_producto(nombre, None) # Busca el producto.
             
             if prod_encontrado:
                 
+                # Solicita el nuevo nombre al usuario.
                 nuevo_nombre = input("\nIngrese el nuevo nombre del producto: ").title()
                 
-                prod_encontrado.set_nombre(nuevo_nombre)
+                prod_encontrado.set_nombre(nuevo_nombre) # Actualiza el nombre del producto.
                 
                 return f"\nEl nombre {nombre} se ha modificado correctamente por {nuevo_nombre}!!"
 
@@ -128,23 +208,35 @@ class Inventario():
         
         except ValueError as e:
             
+            # Maneja excepciones durante la operación.
             print(f"{e}")
     
     def modficar_categorias_inventario(self, categoria):
         
+        """
+        Modifica la categoria de un producto específico.
+        
+        Parámetros:
+            categoria (str): Categoria actual del producto a modificar.
+        
+        Retorno:
+            Mensaje de confirmación de la operación.
+        """
         try:
             
+             # Verifica que el inventario no esté vacío.
             if not self._inventarioProd:
                 
                 raise ValueError("\nEL inventario esta vacio")
             
-            prod_encontrado = self.buscar_producto(None, categoria)
+            prod_encontrado = self.buscar_producto(None, categoria) # Busca el producto.
             
             if prod_encontrado:
                 
+                # Solicita el nuevo nombre al usuario.
                 nueva_categoria = input("\nIngrese la nueva categoria del producto: ").title()
                 
-                prod_encontrado.set_categoria(nueva_categoria)
+                prod_encontrado.set_categoria(nueva_categoria) # Actualiza el nombre de la categoria.
                 
                 return f"\nLa categoria {categoria} se ha modificado correctamente por {nueva_categoria}!!"
 
@@ -152,23 +244,35 @@ class Inventario():
         
         except ValueError as e:
             
+            # Maneja excepciones durante la operación.
             print(f"{e}")
             
     def modificar_precio_producto(self, nombre):
         
-        try:
+        """
+        Modifica el precio de un producto específico.
         
+        Parámetros:
+            precio (float): Nombre actual del producto a modificar.
+        
+        Retorno:
+            Mensaje de confirmación de la operación.
+        """
+        try:
+            
+            # Verifica que el inventario no esté vacío.
             if not self._inventarioProd:
                 
                 raise ValueError("\nEl Producto no esta en el inventario")
             
-            producto_encontrado = self.buscar_producto(nombre, None)
+            producto_encontrado = self.buscar_producto(nombre, None) # Busca el producto.
             
             if producto_encontrado:
                 
+                # Solicita el nuevo precio al usuario.
                 nuevo_precio = float(input("\nIngrese el nuevo precio para el producto: "))
                 
-                producto_encontrado.set_precio(nuevo_precio)
+                producto_encontrado.set_precio(nuevo_precio) # Actualiza el precio del producto.
                 
                 return f"\nEl precio de {nombre} se ha modificado correctamente por {nuevo_precio}!!"
 
@@ -176,9 +280,49 @@ class Inventario():
             
         except ValueError as e:
             
+            # Maneja excepciones durante la operación.
             print(f"{e}")
             
     def modificar_cantidad_producto(self, nombre):
+        
+        """
+        Modifica la cantidad de un producto específico.
+        
+        Parámetros:
+            cantidad (int): Nombre actual del producto a modificar.
+        
+        Retorno:
+            Mensaje de confirmación de la operación.
+        """
+        try:
+            
+            # Verifica que el inventario no esté vacío.
+            if not self._inventarioProd:
+                
+                raise ValueError("\nEl inventario está vacio")
+            
+            producto_encontrado = self.buscar_producto(nombre, None) # Busca el producto.
+            
+            if producto_encontrado:
+                
+                while True:
+                    
+                    try:
+                        nueva_cantidad = int(input("\nIngrese la nueva cantidad para el producto seleccionado: "))
+                        # Llama a set_cantidad para actualizar la cantidad del producto
+                        producto_encontrado.set_cantidad(nueva_cantidad)
+                        return f"\nEl producto {nombre} ha sido modificado correctamente en el inventario."
+                    except ValueError:
+                        print("\nPor favor, ingresa una cantidad válida (un número entero).")
+                
+            else:
+                
+                return f"\nEl producto {nombre} no se ha encontrado en el inventario."
+        
+        except ValueError as e:
+            
+            # Maneja excepciones durante la operación.
+            print(f"{e}") 
         
         try:
         
--- main.py
+++ main.py
@@ -1,47 +1,69 @@
-﻿from ProductosTotales.prod_almacen import ProductosAlmacenados
+﻿# Importación de la clase 'ProductosAlmacenados' desde el módulo 'prod_almacen' dentro del paquete 'Productos'.
+from ProductosTotales.prod_almacen import ProductosAlmacenados
+
+# Importación de la clase 'Inventario' desde el módulo 'manejo_inventario' dentro del paquete 'Inventario'.
 from InventariosTotales.inventarios_productos import Inventario
+
+# Importación del módulo 'platform' que proporciona una interfaz para interactuar con el sistema operativo.
 import platform
+
+# Importación del módulo 'os', que permite interactuar con el sistema operativo, como ejecutar comandos del sistema.
 import os
 
+# Clase para limpiar la pantalla de la consola según el sistema operativo
 class LimpiarPantalla():
     
     def __init__(self):
         
+        # Llama automáticamente al método de limpiar pantalla al instanciar la clase
         self.limpiarpantalla()
         
     def limpiarpantalla(self):
         
+        # Detecta el sistema operativo en el que se está ejecutando el programa
         sisop = platform.system()
         
         if sisop == "Windows":
             
+            # Comando para limpiar la consola en Windows
             os.system("cls")
         
         else:
             
+            # Comando para limpiar la consola en sistemas Unix/Linux/Mac
             os.system("clear")
 
+# Clase que gestiona las operaciones relacionadas con productos heredando de la clase Inventario
 class ManejoProductos(Inventario):
 
+    # Método para agregar un nuevo producto al inventario
     def agregar_productos_nuevos(self):
         
+        # Solicita al usuario el nombre del producto, ajustando a formato título
         nombre = input("\nIngrese el nombre del Producto: ").title()
+        # Solicita al usuario la categoría del producto, ajustando a formato título
         categoria = input("\nIngrese el nombre de la Categoria: ").title()
         
         while True:
         
             try:
                 
+                # Solicita y convierte a float el precio del producto
                 precio = float(input("\nIngrese el Precio del Producto: "))
-    
+
+                # Solicita y convierte a int la cantidad del producto
                 cantidad = int(input("\nIngrese La Cantidad del Producto: "))
-            
+
+                # Crea un objeto de tipo ProductosAlmacenados con los datos ingresados
                 producto =  ProductosAlmacenados(nombre, categoria, precio, cantidad)
                 
+                # Llama al método heredado para agregar el producto al inventario
                 self.agregar_productos_Inventario(producto)
                 
+                # Limpia la pantalla después de agregar el producto
                 LimpiarPantalla()
                 
+                # Muestra un mensaje confirmando que el producto fue agregado exitosamente
                 print(f"\nEl producto \033[1m{nombre}\033[0m se ha agregado correctamente")
                 
                 break
@@ -50,137 +72,177 @@ class ManejoProductos(Inventario):
                 
                 print(f"{e}")
                 
-                return
+                return # Sale del bucle si no hubo errores
         
+    # Método para actualizar el nombre de un producto en el inventario
     def actualizar_nombre(self):
-    
+        
+        # Solicita el nombre actual del producto que se desea modificar
         nombre = input("\nIngrese el nombre del producto a modificar: ").title()
         
+        # Llama al método heredado para modificar el nombre del producto
         resultado = self.modficar_nombres_inventario(nombre)
         
         if resultado:
             
+            # Muestra el resultado si el producto fue encontrado y modificado
             print(f"\n{resultado}")
         
+    # Método para actualizar la categoría de un producto
     def actualizar_categoria(self):
         
+        # Solicita la categoría del producto a modificar
         categoria = input("\nIngrese la categoria a modificar: ").title()
         
+        # Llama al método heredado para modificar la categoría
         resultado = self.modficar_categorias_inventario(categoria)
         
         if resultado:
             
+            # Muestra el resultado de la operación
             print(f"\n{resultado}")
             
+    # Método para actualizar el precio de un producto
     def actualizar_precio_producto(self):
         
+        # Solicita el nombre del producto cuyo precio será actualizado
         nombre = input("\nIngrese el nombre del producto al que va a modificar el precio: ").title()
         
+        # Llama al método heredado para modificar el precio
         resultado = self.modificar_precio_producto(nombre)
         
         if resultado:
             
+            # Muestra el resultado de la operación
             print(f"{resultado}")
             
+    # Método para actualizar la cantidad de un producto en el inventario
     def actualizar_cantidad_producto(self):
-            
+         
+        # Solicita el nombre del producto cuya cantidad será actualizada   
         nombre = input("\nIngrese el nombre del producto al que desea cambiar la cantidad en el inventario: ").title()
         
+        # Llama al método heredado para modificar la cantidad
         resultado = self.modificar_cantidad_producto(nombre)
         
         if resultado:
             
+            # Muestra el resultado de la operación
             print(f"{resultado}")
           
+    # Método para eliminar un producto del inventario
     def eliminar_producto_inventario(self):
         
+        # Solicita el nombre del producto a eliminar
         nombre = input("\nIngresa el nombre del Producto: ").title()
         
+        # Llama al método heredado para eliminar el producto
         self.eliminar_producto(nombre)
      
+    # Método para buscar un producto específico en la lista del inventario
     def buscar_producto_lista(self):
         
         while True:
-        
+            
+            # Solicita el nombre del producto a buscar
             prod = input("\nIngrese el nombre del producto a buscar: ").title()
             
             try:
                 
+                # Llama al método heredado para buscar el producto
                 self.buscar_producto(prod, None)
             
-                break
+                break # Sale del bucle si el producto se encuentra
             
             except ValueError as e:
                 
+                # Captura y muestra el error si el producto no existe
                 print(f"{e}")
                 
-                return
+                return # Finaliza la operación en caso de error
                     
+# Clase para generar y mostrar el menú principal
 class Menu:
-    
+
     def __init__(self):
         
+        # Muestra el encabezado del menú principal
         print("\n\033[1mMenú Para Manejo De Inventario\033[0m\n")
         
+        # Define las opciones del menú
         self.menu = ["Agregar Producto", "Actualizar Producto", "Eliminar Producto", "Mostrar inventario", "Buscar Producto", "Salir del Sistema"]
-        
+
     def mostrar_menu(self):
         
+        # Itera y muestra cada opción con su índice correspondiente
         for i, elemento in enumerate(self.menu, start = 1):
             
             print(f"\033[1m{i}.\033[0m {elemento}")
 
+# Clase para generar y mostrar el submenú de actualizaciones
 class SubMenu:
     
     def __init__(self):
         
+        # Muestra el encabezado del submenú
         print("\n\033[1mMenú Para Actualizar el Inventario\033[0m\n")
         
+        # Define las opciones del submenú
         self.sub_menu = ["Actualizar Nombre", "Actualizar Categoria","Actualizar Precio","actualizar Cantidad","Regresar al Menú Anterior"]
         
     def mostrar_submenu(self):
     
+        # Itera y muestra cada opción del submenú con su índice correspondiente
         for i,n in enumerate(self.sub_menu, start = 1):
             
             print(f"\033[1m{i}.\033[0m {n}")
             
+# Clase principal para controlar la ejecución del programa
 class Main(): 
       
     def __init__(self): 
-           
+        
+        # Crea y muestra el menú principal
         self.mi_menu = Menu()    
         self.mi_menu.mostrar_menu()
-        self.run()   
+        self.run() # Inicia el ciclo principal del programa
          
     def run(self):
         
+        # Instancia ManejoProductos para operar sobre el inventario
         mis_productos = ManejoProductos()
         
         while True:  
               
             try:
-                            
+                
+                # Solicita al usuario una opción del menú principal        
                 opcion = input("\nSeleccione una opción del menú anterior: ")
-                            
+                    
+                # Valida que la opción ingresada sea un número      
                 if not opcion.isnumeric(): 
-                                  
+                                 
                     raise ValueError("\nDebes ingresar un número, la opción no es valida")
                             
                 opcion = int(opcion)
-         
+
+                # Usa match-case para ejecutar la opción seleccionada
                 match opcion: 
                                   
                     case 1:
-                                         
+                        
+                        # Llama al método para agregar productos       
                         mis_productos.agregar_productos_nuevos()
 
                     case 2:
                         
+                        # Limpia pantalla y muestra el submenú
                         LimpiarPantalla()
                         
                         self.misubmenu = SubMenu()
                         self.misubmenu.mostrar_submenu()
                         
+                        # Aquí se maneja el submenú con sus respectivas opciones
                         while True:
                             
                             try:
@@ -213,40 +275,48 @@ class Main():
                                         
                                     case 5:
                                         
+                                        # pregunta al usuario si se queda en el submenu o sale y convierte la respuesta en mayusculas
                                         continuar = input("\n¿Deseas continuar en el submenú? (S/N): ").upper()
                                         
                                         if continuar != "S":
-                                                                
+                                            # Opción para regresar al menú principal
                                             print("\n!Saliendo del Submenú!!\n")
                                             
+                                            # Limpia pantalla y sale
                                             LimpiarPantalla()
                                                 
                                             break
                                     
                                     case _:
                                         
+                                        # lanza una excepcion si no se ha elejido una opcion valida
                                         raise ValueError ("\nDebes elegir un número que este en el menú, intentalo de nuevo")
                                     
                             except ValueError as e:
                                 
+                                # Captura y muestra errores relacionados con la selección del submenú
                                 print(f"{e}")
                                 
                     case 3:                    
                         
+                        # Llama al método para eliminar productos
                         mis_productos.eliminar_producto_inventario()
                     
                     case 4:
                         
+                        # Limpia la pantalla y muestra el inventario completo
                         LimpiarPantalla()
                         
                         mis_productos.mostrar_Inventario()
                         
                     case 5:
                         
+                        # Llama al método para buscar un producto
                         mis_productos.buscar_producto_lista()
                                        
                     case 6:
                         
+                        # limpia la pantalla y Finaliza la ejecución del programa
                         LimpiarPantalla()
                                            
                         print("\n!Saliendo del Sistema, Hasta Pronto!!\n")
@@ -255,16 +325,20 @@ class Main():
                                                             
                     case _:
                         
+                        # lanza una excepcion si no se ha elejido una opcion valida
                         raise ValueError ("\nDebes elegir un número que este en el menú, intentalo de nuevo")
                                     
             except ValueError as e:
-                     
-                print(f"{e}")
                 
+                # Captura y muestra errores relacionados con la selección del menú
+                print(f"{e}")
+            
+            # Muestra el menú principal nuevamente después de cada acción
             print("\n\033[1mMenú Para Manejo De Inventario\033[0m\n")
                 
             self.mi_menu.mostrar_menu()
                       
+# Punto de entrada del programa
 if __name__ == '__main__':
     
     Main()
